f9518cdb8bed6961b087f7b4f8f8a45082079681
Dazzoo <yuraks46@gmail.com>
2023-11-12 18:42:09 +0200

    my bookings fix request

--- app/my-bookings/page.jsx
+++ app/my-bookings/page.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import TitleH1 from '@/components/titles/TitleH1'
 import bookingsApi from '@/api/bookings/bookingsApi';
 import TourCard from '@/components/TourCard'
-import jwtParser from '@/utility/jwtParser';
 
 async function page() {
 
